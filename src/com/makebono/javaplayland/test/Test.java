6e46d4c15cbf6974d2e366e8595a0fd74789b7d4
makebono <xujiant@sfu.ca>
2017-12-08 14:02:03 +0800

    More comments

--- src/com/makebono/javaplayland/test/Test.java
+++ src/com/makebono/javaplayland/test/Test.java
@@ -3,6 +3,8 @@ package com.makebono.javaplayland.test;
 import java.lang.reflect.InvocationTargetException;
 
 import com.makebono.javaplayland.reflection.BonoReflection;
+import com.makebono.javaplayland.tools.entities.OrangeJuice;
+import com.makebono.javaplayland.tools.entities.ReflectOrangeJuice;
 
 /** 
  * @ClassName: Test 
@@ -18,10 +20,19 @@ public class Test {
         final BonoReflection br = new BonoReflection("com.makebono.javaplayland.tools.entities.OrangeJuice");
         System.out.println(br.call("type"));
 
+        final OrangeJuice oj = new OrangeJuice();
+        System.out.println(oj.type());
+
         final BonoReflection br2 = new BonoReflection("com.makebono.javaplayland.tools.entities.ReflectOrangeJuice", 10,
                 "Bono");
 
         br2.call("yell");
         br2.call("advertise", "fantastic");
+
+        final ReflectOrangeJuice roj = new ReflectOrangeJuice();
+        roj.init(10, "Bono");
+
+        roj.yell();
+        roj.advertise("fantastic");
     }
 }
